dcef0d33208b0f08ae0f36e1569de6a46805af48
absiddiq735 <absiddiqchoton735@gmail.com>
2025-03-16 02:07:32 +0800

    Dynamic URL implemented

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tasks.views import show_tasks
+from tasks.views import show_tasks, show_specific_task
 
 urlpatterns=[
-    path('show-tasks/', show_tasks)
+    path('show-tasks/', show_tasks),
+    path('show_tasks/<id>/', show_specific_task)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -10,3 +10,8 @@ def contact(request):
 
 def show_tasks(request):
     return HttpResponse("Here are the tasks")
+
+def show_specific_task(request,id):
+    print('id:',id)
+    print('type:',type(id))
+    return HttpResponse(f"Here is a specific task {id}")
